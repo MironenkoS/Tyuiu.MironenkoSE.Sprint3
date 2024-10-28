c30b47ec738cc24a1fff04fc71cede8560fe0afd
664 <stephencurrytopone@gmail.com>
2024-10-28 19:32:03 +0500

    Task 2

--- Tyuiu.MironenkoSE.Sprint3.Task2.V23.Lib/DataService.cs
+++ Tyuiu.MironenkoSE.Sprint3.Task2.V23.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.MironenkoSE.Sprint3.Task2.V23.Lib
             double sumSeries = 0;
             do
             {
-                sumSeries= sumSeries+((Math.Pow(value,startValue)+1/4)*Math.Sin(startValue))- 0.051;
+                sumSeries= sumSeries+((Math.Pow(value,startValue)+1/4)*Math.Sin(startValue))- 0.0051;
                 startValue++;
 
             } while(startValue<=stopValue);
