71d0311a6559798ae28fcc4e10b9665daf68c7e4
664 <stephencurrytopone@gmail.com>
2024-10-28 19:23:19 +0500

    Task 2

---
+++ Tyuiu.MironenkoSE.Sprint3.Task2.V23.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+using Tyuiu.MironenkoSE.Sprint3.Task2.V23.Lib;
+namespace Tyuiu.MironenkoSE.Sprint3.Task2.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.MironenkoSE.Sprint3.Task2.V23/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.MironenkoSE.Sprint3.Task2.V23.Lib;
+namespace Tyuiu.MironenkoSE.Sprint3.Task2.V23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Тясин И. А. | АСОиУб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла for                                                *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнил: Мироненко Сергей Эдуардович | АСОиУб-24-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу используя цикл do...while, которая вычисляет         *");
+            Console.WriteLine("* произведение ряда по формуле, при a = 1.5                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double value = 5;
+            int startValue = 1;
+            int stopValue = 10;
+            Console.WriteLine("Переменная x=" + value);
+            Console.WriteLine("Старт Шага=" + startValue);
+            Console.WriteLine("Конец Шага=" + stopValue);
+
+
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Cумма ряда ="+ds.GetSumSeries(value, startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
